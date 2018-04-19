d5e1f7ad32202314c12f0b8c580031d3975e9cd1
tomtonthat <37990548+tomtonthat@users.noreply.github.com>
2018-04-18 21:29:18 -0600

    Gate Locations

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 	<h1>Sandia Labs Travel Duration App</h1>
 	<div data-role="navbar">
 		<ul>
-			<li><a href="#homePage">Geolocation</a></li>
+			<li><a href="#homePage">Travel Times</a></li>
 			<li><a href="#mapPage">Traffic Map</a></li>
 			<li><a href="#directionsPage">Directions</a></li>
 		</ul>
--- index.php
+++ index.php
@@ -13,9 +13,8 @@
 	<?php include("header.php") ?>
 
 	<div role="main" class="ui-content">
-		<p>Geolocation Page</p>
-		<div id="geoLocation">geoLocation</div>
-		<button id="getGeolocation">get geolocation</button>
+		<p>Travel Times</p>
+		<button id="getTravelTimes">Get Travel Times</button>
 	</div><!-- /content -->
 
 	<?php include("footer.php") ?>
@@ -44,6 +43,7 @@
 
 	<?php include("footer.php") ?>
 </div><!-- /page -->
+
     <script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
     <script src="//code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCTZea67jn4YSPIGu0dNTHRyB1jnvo1Q00"></script>
