4a261da30d074da7e13b5f46682a9fac938516cd
tomtonthat <37990548+tomtonthat@users.noreply.github.com>
2018-04-24 19:11:47 -0600

    Updated callback to match google documentation

--- geolocation.js
+++ geolocation.js
@@ -121,16 +121,23 @@ $(document).ready(function () {
     }, callback);
   
     function callback(response, status) {
-      if (status !== google.maps.DistanceMatrixStatus.OK) {
-          console.log('Error:', status);
-      } else {
-          console.log(response);
-          $("#distance").text(response.rows[0].elements[0].distance.text).show();
-          $("#duration").text(response.rows[0].elements[0].duration.text).show();
-      }
-    }   
-      
+      if (status == 'OK') {
+        var origins = response.originAddresses;
+        var destinations = response.destinationAddresses;
     
+        for (var i = 0; i < origins.length; i++) {
+          var results = response.rows[i].elements;
+          for (var j = 0; j < results.length; j++) {
+            var element = results[j];
+            var distance = element.distance.text;
+            var duration = element.duration.text;
+            var from = origins[i];
+            var to = destinations[j];
+            console.log(response);
+          }
+        }
+      }
+    }
   }
     $('#getTravelTimes').on('click', function(){
       console.log("#getTravelTimes Clicked");
