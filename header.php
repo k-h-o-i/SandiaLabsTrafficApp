79dd284e6fe869bd16ff6d297313ae0d47b1b081
tomtonthat <37990548+tomtonthat@users.noreply.github.com>
2018-04-25 20:20:32 -0600

    Updated header

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <div data-role="header">
-	<h1>Sandia Labs Travel Duration App</h1>
+	<h1>Kirtland AFB Traffic App</h1>
 	<div data-role="navbar">
 		<ul>
 			<li><a href="#homePage">Travel Times</a></li>
--- index.php
+++ index.php
@@ -33,8 +33,6 @@
 	<?php include("footer.php") ?>
 </div><!-- /page -->
 
-
-
     <script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
     <script src="//code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCTZea67jn4YSPIGu0dNTHRyB1jnvo1Q00"></script>
